56e39f14f1618b5e7bd7fce763b69b29d98a4b47
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-04-22 16:41:29 -0300

    Funcionalidade de colocar tarefas em array - desenvolvida

--- src/pages/HomePage/InputTask/InputTask.js
+++ src/pages/HomePage/InputTask/InputTask.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './InputTask.module.scss';
 import Button from '../../../components/Button/Button';
 
 const InputTask = () => {
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [task, setTask] = useState('');
+  const [listTask, setListTask] = useState([]);
+  const input = useRef();
+
+  const handleInputChange = ({ target }) => {
+    setTask(target.value);
+    setErrorMsg(false);
+  }
+
+  // console.log(listTask)
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (task) {
+      setListTask([...listTask, task]);
+      setTask('');
+      input.current.focus();
+      
+    } else {
+      setErrorMsg('Você precisa escrever uma tarefa.')
+    }
+  }
+
+  useEffect(() => {
+    input.current.focus();
+  }, []);
+
   return (
-    <form className={styles.formCreateTask} onSubmit={(e) => e.preventDefault()}>
-      <input type="text" placeholder='Escreva uma nova tarefa...' />
+    <form className={styles.formCreateTask} onSubmit={handleSubmit}>
+      <input 
+        ref={input}
+        type="text" 
+        placeholder='Escreva uma nova tarefa...' 
+        value={task}
+        onChange={handleInputChange}
+      />
+      {errorMsg && <p>{errorMsg}</p>}
       <Button className={styles.sendNewTask}>
         Adicionar nova tarefa
       </Button>
