7a22f26879fd888435f0dc89b560011abda8cd82
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-04-23 21:59:28 -0300

    Área onde as tarefas são listadas - desenvolvida

--- src/pages/HomePage/List/List.js
+++ src/pages/HomePage/List/List.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './List.styles';
 import Theme from '../../../styles/Theme';
 import Button from '../../../components/Button/Button';
 
 const List = ({ taskList, setTaskList }) => {
-  if (taskList.length) {
-    console.log(taskList);
+  const [category, setCategory] = useState('all');
+
+  const handleButtonClick = ({ target }) => {
+    if (target.id === 'all') setCategory(target.id);
+    if (target.id === 'done') setCategory(target.id);
+    if (target.id === 'todo') setCategory(target.id);
   }
+
+  const filterTaskList = taskList.length > 0 && taskList.filter(taskFilter => {
+    return taskFilter.defaultCategory === category || taskFilter.category === category;
+  });
 
   return (
     <S.ListContainer>
       <h3>Lista de Tarefas</h3>
       <S.ButtonsFilter>
         <Button
+          onClick={handleButtonClick}
           id='all' 
           width='100%' 
           bg={Theme.colors.GreenColor}
@@ -22,6 +31,7 @@ const List = ({ taskList, setTaskList }) => {
         </Button>
 
         <Button
+          onClick={handleButtonClick}
           id='done' 
           width='100%' 
           bg={Theme.colors.GreenColor}
@@ -31,6 +41,7 @@ const List = ({ taskList, setTaskList }) => {
         </Button>
         
         <Button
+          onClick={handleButtonClick}
           id='todo' 
           width='100%' 
           bg={Theme.colors.GreenColor}
@@ -39,6 +50,16 @@ const List = ({ taskList, setTaskList }) => {
           Incompletas
         </Button>
       </S.ButtonsFilter>
+
+      <S.TasksContainer>
+        {filterTaskList && filterTaskList.map((task, index) => (
+          <React.Fragment key={`task ${index}`}>
+            <li id={index}>
+              {task.task}
+            </li>
+          </React.Fragment>
+        ))}
+      </S.TasksContainer>
     </S.ListContainer>
   )
 }
--- src/pages/HomePage/List/List.styles.js
+++ src/pages/HomePage/List/List.styles.js
@@ -1,4 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const animationList = keyframes`
+  to {
+    opacity: 1;
+  }
+`;
 
 export const ListContainer = styled.div`
   padding-top: 1.25rem;
@@ -15,4 +21,21 @@ export const ButtonsFilter = styled.div`
   padding-top: .75rem;
   display: flex;
   gap: 1.5rem;
+`;
+
+export const TasksContainer = styled.ul`
+  padding-top: 2rem;
+
+  li {
+    opacity: 0;
+    padding: .5rem 1rem;
+    animation: ${animationList} forwards .4s;
+    ${({ theme }) => css`
+      border: 1px solid ${theme.colors.BorderColor};
+    `}
+
+    &:not(:last-of-type) {
+      margin-bottom: .75rem;
+    }
+  }
 `;
